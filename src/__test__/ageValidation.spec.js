80aa287b1d47fcde9065637722cf56d0bd2192c4
Ondrej Bartas <ondrej@bartas.cz>
2016-11-02 00:01:10 +0100

    Extract get age

--- src/__test__/ageValidation.spec.js
+++ src/__test__/ageValidation.spec.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import test from 'ava';
-import ageValidation from '../ageValidation';
+import ageValidation, { getAge } from '../ageValidation';
 
 function getPersonalID(old, girl = false) {
   const base = moment().subtract(old, 'years').format('YYMMDD1234');
@@ -18,10 +18,6 @@ test('ageValidation should return error when age is under 18', t =>
   t.is(ageValidation(getPersonalID(17)), 'under 18')
 );
 
-test('ageValidation should return error when age is under 18 and id is girl', t =>
-  t.is(ageValidation(getPersonalID(17, true)), 'under 18')
-);
-
 test('ageValidation should not return error when age is exactly 18', t =>
   t.is(ageValidation(getPersonalID(18)), null)
 );
@@ -29,3 +25,27 @@ test('ageValidation should not return error when age is exactly 18', t =>
 test('ageValidation should not return error when age is over 18', t =>
   t.is(ageValidation(getPersonalID(21)), null)
 );
+
+test('ageValidation should return error when age is under 18 and id is girl', t =>
+  t.is(ageValidation(getPersonalID(17, true)), 'under 18')
+);
+
+test('ageValidation should not return error when girls age is over 18', t =>
+  t.is(ageValidation(getPersonalID(20, true)), null)
+);
+
+test('getAge for boy', (t) => {
+  t.is(getAge(getPersonalID(5)), 5);
+  t.is(getAge(getPersonalID(17)), 17);
+  t.is(getAge(getPersonalID(20)), 20);
+  t.is(getAge(getPersonalID(65)), 65);
+  t.is(getAge(getPersonalID(95)), 95);
+});
+
+test('getAge for girl', (t) => {
+  t.is(getAge(getPersonalID(5, true)), 5);
+  t.is(getAge(getPersonalID(17, true)), 17);
+  t.is(getAge(getPersonalID(20, true)), 20);
+  t.is(getAge(getPersonalID(65, true)), 65);
+  t.is(getAge(getPersonalID(95, true)), 95);
+});
--- src/ageValidation.js
+++ src/ageValidation.js
@@ -4,13 +4,12 @@ import moment from 'moment';
 export function getAge(personalId) {
   const year = parseInt(personalId.substr(0, 2), 10);
   // month % 50 will remove information about girl from ID
-  const month = parseInt(personalId.substr(2, 2), 10) % 50;
+  const month = parseInt(personalId.substr(2, 2), 10);
   const day = parseInt(personalId.substr(4, 2), 10);
   const century = year <= moment().year() - 2000 ? 20 : 19;
-
-  return moment(`${century}${year}-${month}-${day}`);
+  return moment().diff(moment(`${century}${year}-${month}-${day}`), 'years');
 }
 
 export default function ageValidation(personalId) {
-  return (moment().diff(getAge(personalId), 'years') < 18) ? 'under 18' : null;
+  return (getAge(personalId) < 18) ? 'under 18' : null;
 }
