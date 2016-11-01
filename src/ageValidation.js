639fe4ecc84c550acd4eedf026a2364debd1a354
Ondrej Bartas <ondrej@bartas.cz>
2016-11-02 00:15:42 +0100

    Fix tests for getBirth

--- src/ageValidation.js
+++ src/ageValidation.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 export function getBirth(personalId) {
   const year = parseInt(personalId.substr(0, 2), 10);
   // month % 50 will remove information about girl from ID
-  const month = parseInt(personalId.substr(2, 2), 10);
+  const month = parseInt(personalId.substr(2, 2), 10) % 50;
   const day = parseInt(personalId.substr(4, 2), 10);
   const century = year <= moment().year() - 2000 ? 20 : 19;
   return `${century}${year}-${month}-${day}`;
